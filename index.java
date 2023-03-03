b65f90f0b3c15b38ca26d38433fcaaf107ea0abf
shapuramharsha <shapuramharsha02@gmail.com>
2023-03-03 18:05:31 +0530

    First Commit

---
+++ index.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; // This will import just the Scanner class
+import java.util.*; 
+
+// Your First Program
+
+class index {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
